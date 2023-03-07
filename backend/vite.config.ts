9b8e31ed460ad8b8c47665f9fe53fed8fae64ed6
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2023-03-07 12:52:11 +0000

    format

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
-    server: {
-        port: 4002
-    },
-    plugins: [
-        ...VitePluginNode({
-            adapter: 'express',
-            appPath: './src/pothos-app.ts',
-            exportName: 'viteNodeApp',
-            tsCompiler: 'swc',
-        })
-    ],
-    optimizeDeps: {
-        exclude: [
-        ],
-    },
+  server: {
+    port: 4002,
+  },
+  plugins: [
+    ...VitePluginNode({
+      adapter: 'express',
+      appPath: './src/pothos-app.ts',
+      exportName: 'viteNodeApp',
+      tsCompiler: 'swc',
+    }),
+  ],
+  optimizeDeps: {
+    exclude: [],
+  },
 });
